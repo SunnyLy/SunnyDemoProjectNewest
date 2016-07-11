d1ec5323c8e7b5afa249704e0fa9843753aa05c5
SunnyLy <450512747@qq.com>
2016-07-11 20:05:09 +0800

    添加OpenGL ES2.0功能，目前手势监听还未完成

---
+++ SunnyDemo/src/sunnydemo2/opengl/SunnyGLRender.java
@@ -0,0 +1,9 @@
+package sunnydemo2.opengl;
+
+/**
+ * @Author sunny
+ * @Date 2016/7/11  13:31
+ * @Annotation
+ */
+public class SunnyGLRender {
+}
---
+++ SunnyDemo/src/sunnydemo2/opengl/SunnyOpenGLActivity.java
@@ -0,0 +1,9 @@
+package sunnydemo2.opengl;
+
+/**
+ * @Author sunny
+ * @Date 2016/7/11  13:25
+ * @Annotation
+ */
+public class SunnyOpenGLActivity {
+}
